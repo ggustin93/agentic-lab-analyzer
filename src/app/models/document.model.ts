69c37c46acef51dc38c59f87d014f2721a234cbc
Guillaume Gst <guillaume.gustin.9@gmail.com>
2025-07-01 23:59:54 +0200

    Updated .env.example

---
+++ src/app/models/document.model.ts
@@ -0,0 +1,36 @@
+export interface HealthDocument {
+  id: string;
+  title: string;
+  filename: string;
+  uploadedAt: Date;
+  status: DocumentStatus;
+  extractedData?: ExtractedHealthData[];
+  aiInsights?: string;
+  rawText?: string;
+  errorMessage?: string;
+}
+
+export interface ExtractedHealthData {
+  marker: string;
+  value: string;
+  unit?: string;
+  referenceRange?: string;
+}
+
+export enum DocumentStatus {
+  PROCESSING = 'processing',
+  COMPLETE = 'complete',
+  ERROR = 'error'
+}
+
+export interface UploadResponse {
+  documentId: string;
+  message: string;
+}
+
+export interface AnalysisResult {
+  documentId: string;
+  extractedData: ExtractedHealthData[];
+  aiInsights: string;
+  rawText: string;
+}
